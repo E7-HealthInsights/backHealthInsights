d296eec3b57afa69d40fa707014a603b63ce00e8
Santiago Niño <a01665906@tec.mx>
2026-04-17 18:03:08 -0600

    Integracion de Firebase, para autenticacion de usuarios

---
+++ src/main/java/org/acme/infrastructure/config/FirebaseConfig.java
@@ -0,0 +1,32 @@
+package org.acme.infrastructure.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import io.quarkus.runtime.Startup;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@Startup
+@ApplicationScoped
+public class FirebaseConfig {
+    @ConfigProperty(name="firebase.credentials")   //es un json que es mi llave privada del servidor
+    private String path;
+
+    void onStart(@Observes StartupEvent ev) throws FileNotFoundException {
+        try {
+            if(FirebaseApp.getApps().isEmpty()){
+                try(InputStream serviceAccount = new FileInputStream(path)){
+                    FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+                    FirebaseApp.initializeApp(options);   //conexion backend-firebase
+                }
+            }
+        } catch (Exception e){}
+    }
+}
