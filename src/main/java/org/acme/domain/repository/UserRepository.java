7bbbe75b1d6208b6c8ad071cfd40c87b4437d1bf
Santiago Niño <a01665906@tec.mx>
2026-04-17 18:09:22 -0600

    Seguridad, protegiendo los endpoints con tokens para expiracion, y filter de firebase

--- src/main/java/org/acme/domain/repository/UserRepository.java
+++ src/main/java/org/acme/domain/repository/UserRepository.java
@@ -2,7 +2,10 @@ package org.acme.domain.repository;
 
 import org.acme.domain.models.User;
 
+import java.util.Optional;
+
 public interface UserRepository {
 
     User create(User user);
+    Optional<User> findByFirebaseUuid(String firebaseUuid);
 }
---
+++ src/main/java/org/acme/infrastructure/security/FirebaseAuthFilter.java
@@ -0,0 +1,69 @@
+package org.acme.infrastructure.security;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import jakarta.annotation.Priority;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Priorities;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+import org.acme.domain.models.User;
+import org.acme.domain.repository.UserRepository;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Provider
+@Priority(Priorities.AUTHENTICATION)
+public class FirebaseAuthFilter implements ContainerRequestFilter {
+
+    @Inject
+    UserRepository userRepository;
+    @Inject
+    AuthContext authContext;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        String path=requestContext.getUriInfo().getPath();
+        System.out.println("Path: "+path);
+        if (path.equals("/users")){
+            return;   //si la ruta es /users, no se requiere autenticación (registro de usuario)
+        }
+
+        if(path.startsWith("/q/")){
+            return;
+        }
+
+        String authHeader = requestContext.getHeaders().getFirst("Authorization");   //se obtiene el header de autorización de la solicitud
+
+        if(authHeader == null){
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("No autorizado").build());
+            return;  //si no hay header de autorización, se aborta la solicitud con un error
+        }
+
+        if(!authHeader.startsWith("Bearer ")){
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("No autorizado").build());
+            return;  //si el header no tiene el formato correcto, se aborta la solicitud con un error
+        }
+
+        String token = authHeader.substring("Bearer ".length());  //se extrae el token del header de autorización
+
+        try{
+            FirebaseToken decodedToken = FirebaseAuth.getInstance().verifyIdToken(token, true);
+            // firebase dice: token valido, usuario real
+            Optional<User> userOptional = userRepository.findByFirebaseUuid(decodedToken.getUid());  //se busca el usuario en la base de datos usando el UUID de Firebase
+            if(userOptional.isEmpty()){
+                requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("No autorizado").build());
+                return;  //si no se encuentra el usuario en la base de datos, se aborta la solicitud con un error
+            }
+            User user = userOptional.get();
+            authContext.setUser(user);
+            //si se encuentra el usuario, se establece en el contexto de autenticación para que esté disponible en los recursos protegidos
+        } catch (FirebaseAuthException e){
+            requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).entity("No autorizado").build());
+        };
+    }
+}
