d1024d1c484e360e75b9529d6ea0482aaf9f6b8f
Santiago Niño <a01665906@tec.mx>
2026-05-17 23:25:47 -0600

    Creacion de tabla Proyeccion en BD MySQL

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -57,6 +57,20 @@ CREATE TABLE Widget (
                         FOREIGN KEY (tipo_id) REFERENCES Tipo_de_Grafica(id)
 );
 
+-- Tabla Proyeccion
+CREATE TABLE Proyeccion (
+    id VARCHAR(36) PRIMARY KEY,
+    title VARCHAR(100),
+    descripcion text,
+    usuario_id VARCHAR(36),
+    query text,
+    fecha_creacion DATETIME,
+    fecha_actualizacion DATETIME,
+
+    FOREIGN KEY (usuario_id) REFERENCES Users(id)
+);
+
+
 
 -- ── Roles ─────────────────────────────────────────────────────────────────────
 
