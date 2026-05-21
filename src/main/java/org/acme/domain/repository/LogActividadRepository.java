a9ff16b019377196032eef2034289b79c63fc952
Santiago Niño <a01665906@tec.mx>
2026-05-20 21:52:39 -0600

    Metodos de LogActividad para traer el log mas reciente en base al tipo de entidad y actualizar su detalle en base a lo enviado en el POST/PATCH/PUT

--- src/main/java/org/acme/domain/repository/LogActividadRepository.java
+++ src/main/java/org/acme/domain/repository/LogActividadRepository.java
@@ -3,7 +3,11 @@ package org.acme.domain.repository;
 import org.acme.domain.models.LogActividad;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface LogActividadRepository {
     List<LogActividad> findAllLogs();
+    Optional<LogActividad> findLatestByEntidadId(String entidadId);
+    void updateDetalle(UUID logid, String detalle);
 }
